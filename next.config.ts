c0c8219f3e3ae196bbe39696418dd5a3ec443bad
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-10-23 18:00:15 +0600

    all ok

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // for Vercel hosting
+  trailingSlash: false, // optional
 };
 
 export default nextConfig;
