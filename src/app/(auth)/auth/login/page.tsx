d917f566181c725e6cc376f5c274bb310884d73d
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2026-01-22 10:02:49 +0600

    auth pagfe responsive ok

--- src/app/(auth)/auth/login/page.tsx
+++ src/app/(auth)/auth/login/page.tsx
@@ -45,7 +45,7 @@ export const metadata: Metadata = {
 
 function page() {
   return (
-    <div className="w-full min-h-screen bg-transparent lg:bg-gray-50 flex items-center justify-center p-4 sm:p-6 lg:p-8">
+    <div className="w-full  bg-transparent lg:bg-gray-50 flex items-center justify-center p-4 sm:p-6 lg:p-8">
       <Login />
     </div>
   );
--- src/app/(auth)/auth/reset-password/page.tsx
+++ src/app/(auth)/auth/reset-password/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function page() {
   return (
-    <div className="w-full min-h-screen bg-transparent lg:bg-gray-50 flex items-center justify-center p-4 sm:p-6 lg:p-8">
+    <div className="w-full  bg-transparent lg:bg-gray-50 flex items-center justify-center p-4 sm:p-6 lg:p-8">
       <ResetPassword />
     </div>
   );
